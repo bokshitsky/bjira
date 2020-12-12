63c09b214ffd28005eedd6f322e48b7abcfef3a2
boksh <e.bokshitskiy@hh.ru>
2020-12-12 13:53:22 +0300

    simplify function

--- bjira/operations/view.py
+++ bjira/operations/view.py
@@ -1,4 +1,5 @@
 import webbrowser
+
 import git
 
 from bjira.operations import BJiraOperation
@@ -12,13 +13,16 @@ class ViewJiraTask(BJiraOperation):
         parser.set_defaults(func=self._view_jira_task)
 
     def _view_jira_task(self, args):
-        task_name = args.task_name
-        if task_name is None:
-            try:
-                repo = git.Repo('.')
-                task_name = repo.active_branch.name
-            except git.exc.InvalidGitRepositoryError:
-                print('you have to specify task_name or to be in a git-repository')
-                return
-        print(f'opening task https://jira.hh.ru/browse/{task_name}')
-        webbrowser.open(f"https://jira.hh.ru/browse/{task_name}")
+        task_name = self._get_task_name(args)
+        if task_name:
+            print(f'opening task https://jira.hh.ru/browse/{task_name}')
+            webbrowser.open(f"https://jira.hh.ru/browse/{task_name}")
+
+    def _get_task_name(self, args):
+        if args.task_name is not None:
+            return args.task_name
+        try:
+            repo = git.Repo('.')
+            return repo.active_branch.name
+        except git.exc.InvalidGitRepositoryError:
+            print('you have to specify task_name or to be in a git-repository')
