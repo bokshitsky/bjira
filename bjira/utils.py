79d868b22aaf27c459b86ce44e20d665a842f3e7
Alexander Mishurov <a.mishurov@hh.ru>
2025-03-25 16:54:28 +0300

    Add img prefix for search output

--- bjira/utils.py
+++ bjira/utils.py
@@ -2,6 +2,16 @@ import re
 
 JIRA_SERVICE = 'bjira'
 
+IMG_STATUS_PREFIX = {
+    'Closed': '✅',
+    'Resolved': '✅',
+    'Released': '✅',
+    'Fixed': '✅',
+    'Open': '⭕️',
+    'In Progress': '⭕️',
+    'Need Review': '⭕️',
+}
+
 
 def parse_portfolio_task(portfolio):
     return 'PORTFOLIO-' + re.sub('[^0-9]', '', portfolio)
